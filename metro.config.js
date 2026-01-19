85f84e49a07f326a0abd8a4ff92e1fb78edaea87
natively.dev <api@natively.dev>
2026-01-19 16:01:37 +0000

    fix: Simplify Metro config for Expo SDK 54 compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,6 @@
 
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-// Enable package exports for better module resolution
-config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
