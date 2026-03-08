1dc1ce2a242da8233d3651b9b06fc900868d9714
natively.dev <api@natively.dev>
2026-03-08 10:27:33 +0000

    fix: Correct malformed Gradle config in Stripe plugin

--- plugins/withStripeFixed.js
+++ plugins/withStripeFixed.js
@@ -1,11 +1,39 @@
 
 const { withProjectBuildGradle } = require('@expo/config-plugins');
+
 module.exports = (config) => {
-return withProjectBuildGradle(config, (config) => {
-config.modResults.contents = config.modResults.contents.replace(
-/allprojects\s*{[\s\S]*?^}/m,
-`allprojects { repositories { exclusiveContent { forRepository { mavenCentral() } filter { includeGroup "com.stripe" } } google() mavenCentral() } configurations.all { resolutionStrategy { eachDependency { details -> if (details.requested.group == 'com.stripe') { if (details.requested.name == 'stripe-android' || details.requested.name == 'financial-connections') { details.useVersion '20.51.0' } } } capabilitiesResolution.withCapability('com.google.guava:listenablefuture') { select('com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava') } } } }`
-);
-return config;
-});
+  return withProjectBuildGradle(config, (config) => {
+    config.modResults.contents = config.modResults.contents.replace(
+      /allprojects\s*{[\s\S]*?^}/m,
+      `allprojects {
+    repositories {
+        exclusiveContent {
+            forRepository {
+                mavenCentral()
+            }
+            filter {
+                includeGroup "com.stripe"
+            }
+        }
+        google()
+        mavenCentral()
+    }
+    configurations.all {
+        resolutionStrategy {
+            eachDependency { details ->
+                if (details.requested.group == 'com.stripe') {
+                    if (details.requested.name == 'stripe-android' || details.requested.name == 'financial-connections') {
+                        details.useVersion '20.51.0'
+                    }
+                }
+            }
+            capabilitiesResolution.withCapability('com.google.guava:listenablefuture') {
+                select('com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava')
+            }
+        }
+    }
+}`
+    );
+    return config;
+  });
 };
